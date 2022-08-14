9439c883ca3cf932e05ccc9b933b44a9376896bd
Rogerio Leal <rogerioleal1@gmail.com>
2022-08-14 19:18:12 -0300

    feat: Add solveMeFirst problem

---
+++ solve-me-first.php
@@ -0,0 +1,11 @@
+<?php
+
+// https://www.hackerrank.com/challenges/solve-me-first/problem
+
+function solveMeFirst($numberOne, $numberTwo)
+{
+    // Hint: Type return $numberOne + $numberTwo; below
+    return $numberOne + $numberTwo;
+}
+
+echo solveMeFirst(10, 20);

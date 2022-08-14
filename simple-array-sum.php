a37cf6a9396f6c05ca83e5e3c041bf6aad5ed434
Rogerio Leal <rogerioleal1@gmail.com>
2022-08-14 19:46:10 -0300

    refactor: reorder functions in simple-array-sum.php

--- simple-array-sum.php
+++ simple-array-sum.php
@@ -2,16 +2,6 @@
 
 // https://www.hackerrank.com/challenges/simple-array-sum/problem
 
-function simpleArraySumForeach($array)
-{
-    $arraySum = 0;
-    foreach ($array as $row) {
-        $arraySum += $row;
-    }
-
-    return $arraySum;
-}
-
 function simpleArraySumFor($array)
 {
     [$arraySum, $arraySize] = [0, count($array)];
@@ -22,6 +12,16 @@ function simpleArraySumFor($array)
     return $arraySum;
 }
 
+function simpleArraySumForeach($array)
+{
+    $arraySum = 0;
+    foreach ($array as $row) {
+        $arraySum += $row;
+    }
+
+    return $arraySum;
+}
+
 echo array_sum([1, 2, 3, 4, 10, 11]);
-echo simpleArraySumForeach([1, 2, 3, 4, 10, 11]);
 echo simpleArraySumFor([1, 2, 3, 4, 10, 11]);
+echo simpleArraySumForeach([1, 2, 3, 4, 10, 11]);

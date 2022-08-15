2f34f50146873b1f23d26ba4e6850570ca3142b6
Rogerio Leal <rogerioleal1@gmail.com>
2022-08-15 12:29:49 -0300

    feat: Add time conversion problem

---
+++ time-conversion.php
@@ -0,0 +1,23 @@
+<?php
+
+// https://www.hackerrank.com/challenges/time-conversion/problem
+
+function timeConversion($time)
+{
+    [$hour, $minutes, $seconds] = explode(':', $time);
+
+    $isPM = strpos($time, 'PM');
+    if ($isPM === false) {
+        $hour = $hour === '12' ? '00' : $hour;
+    } else {
+        $hour = ($hour + 12) >= 24 ? '12' : $hour + 12;
+    }
+
+    $time = implode(':', [$hour, $minutes, $seconds]);
+
+    return str_replace(['AM', 'PM'], '', $time);
+}
+
+echo timeConversion('07:45:54AM') . PHP_EOL;
+echo timeConversion('07:45:54PM') . PHP_EOL;;
+echo timeConversion('12:45:54PM') . PHP_EOL;;

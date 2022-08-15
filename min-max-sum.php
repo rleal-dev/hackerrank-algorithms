b04378a2802ef66038bfa1be5b5f4e20b72832e2
Rogerio Leal <rogerioleal1@gmail.com>
2022-08-14 21:01:55 -0300

    feat: Add min max sum problem

---
+++ min-max-sum.php
@@ -0,0 +1,25 @@
+<?php
+
+function miniMaxSum($array)
+{
+    [$sum, $minSum, $maxSum, $arraySize] = [0, 0, 0, count($array)];
+    for ($i = 0; $i < $arraySize; $i++) {
+        $sum += $array[$i];
+    }
+
+    for ($i = 0; $i < $arraySize; $i++) {
+        $diffSum = $sum - $array[$i];
+
+        if ($minSum == 0 || $diffSum < $minSum) {
+            $minSum = $diffSum;
+        }
+
+        if ($diffSum > $maxSum) {
+            $maxSum = $diffSum;
+        }
+    }
+
+    echo $minSum . ' ' . $maxSum;
+}
+
+miniMaxSum([1, 3, 5, 7, 9]);
